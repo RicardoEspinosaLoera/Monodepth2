b0cc454219d24f0693336943e7ba63963ba710ea
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2024-02-07 20:09:12 -0600

    monovit

--- networks/__init__.py
+++ networks/__init__.py
@@ -2,3 +2,6 @@ from .resnet_encoder import ResnetEncoder
 from .depth_decoder import DepthDecoder
 from .pose_decoder import PoseDecoder
 from .pose_cnn import PoseCNN
+from .hr_decoder import DepthDecoderT
+from .mpvit import *
+from .nets import DeepNet
