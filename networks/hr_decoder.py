31a3ae9725337990500c7424d8be7fe46c0e6e1a
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2024-02-07 20:32:17 -0600

    AdamW

--- networks/hr_decoder.py
+++ networks/hr_decoder.py
@@ -8,6 +8,7 @@ from .hr_layers import *
 
 
 class DepthDecoderT(nn.Module):
+    #def __init__(self, ch_enc, scales=range(4),num_ch_enc = [ 64, 64, 128, 256, 512 ], num_output_channels=1):
     def __init__(self, ch_enc = [64,128,216,288,288], scales=range(4),num_ch_enc = [ 64, 64, 128, 256, 512 ], num_output_channels=1):
         super(DepthDecoderT, self).__init__()
         self.num_output_channels = num_output_channels
@@ -86,6 +87,8 @@ class DepthDecoderT(nn.Module):
     def forward(self, input_features):
         outputs = {}
         feat={}
+        #print(len(input_features))
+        #print(input_features[4].shape)
         feat[4] = self.convs["f4"](input_features[4])
         feat[3] = self.convs["f3"](input_features[3])
         feat[2] = self.convs["f2"](input_features[2])
--- trainer.py
+++ trainer.py
@@ -115,7 +115,7 @@ class Trainer:
             self.models["predictive_mask"].to(self.device)
             self.parameters_to_train += list(self.models["predictive_mask"].parameters())
 
-        self.model_optimizer = optim.Adam(self.parameters_to_train, self.opt.learning_rate)
+        self.model_optimizer = optim.AdamW(self.parameters_to_train, self.opt.learning_rate)
         self.model_lr_scheduler = optim.lr_scheduler.StepLR(
             self.model_optimizer, self.opt.scheduler_step_size, 0.1)
 
