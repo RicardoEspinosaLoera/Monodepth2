f1afe058d919d98f6f569d1b7cc82e0b0b7d3720
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2024-03-13 13:22:35 -0600

    hamlyn

--- datasets/scared_dataset.py
+++ datasets/scared_dataset.py
@@ -73,7 +73,7 @@ class SCAREDDataset(MonoDataset):
         folder = line[0]
 
         if len(line) == 3:
-            frame_index = int(line[1])
+            frame_index = line[1]
         else:
             frame_index = 0
 
@@ -101,12 +101,16 @@ class SCAREDRAWDataset(SCAREDDataset):
 
     def get_image_path(self, folder, frame_index, side):
         #SCATER
-        f_str = "{}{}".format(frame_index, self.img_ext)
-        image_path = os.path.join(self.data_path, folder, "data", f_str)
+        """f_str = "{}{}".format(frame_index, self.img_ext)
+        image_path = os.path.join(self.data_path, folder, "data", f_str)"""
         
         #COLON10k
         #f_str=str(frame_index) + self.img_ext
         #image_path = os.path.join(self.data_path, folder, f_str)
+
+        f_str = "{}{}".format(frame_index, self.img_ext)
+        folder1,folder2 = folder.split("/")
+        image_path = os.path.join(self.data_path, folder1,folder1,folder2, f_str)
             
         return image_path
 
--- options.py
+++ options.py
@@ -181,9 +181,9 @@ class MonodepthOptions:
                                  help="optional path to a .npy disparities file to evaluate")
         self.parser.add_argument("--eval_split",
                                  type=str,
-                                 default="endovis",
+                                 default="hamlyn",
                                  choices=[
-                                    "eigen", "eigen_benchmark", "benchmark", "odom_9", "odom_10","endovis"],
+                                    "eigen", "eigen_benchmark", "benchmark", "odom_9", "odom_10","endovis","hamlyn"],
                                  help="which split to run eval on")
         self.parser.add_argument("--save_pred_disps",
                                  help="if set saves predicted disparities",
